7b08820181a60baa4d841765f20ecd408b26ac43
jgellatly5 <jgellatly5@gmail.com>
2016-11-09 13:03:03 -0800

    initial commit
    initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,67 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by jgell on 10/25/2016.
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+    private String mSuspect;
+
+    public Crime() {
+        // Generate unique identifier
+        this(UUID.randomUUID());
+    }
+
+    public Crime(UUID id) {
+        mId = id;
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
+    }
+
+    public String getPhotoFilename() {
+        return "IMG " + getId().toString() + ".jpg";
+    }
+}
